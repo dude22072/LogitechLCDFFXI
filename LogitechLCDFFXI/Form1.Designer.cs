126bdb387f762d3afd995930672fd6e8b7c45ff5
dude22072 <josephkeller1999@icloud.com>
2016-05-01 22:51:00 -0500

    Initial Commit

---
+++ LogitechLCDFFXI/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace LogitechLCDFFXI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.rb_notify_true = new System.Windows.Forms.RadioButton();
+            this.rb_notify_false = new System.Windows.Forms.RadioButton();
+            this.timerUpdate = new System.Windows.Forms.Timer(this.components);
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblNotify = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.Text = "FFXI LCD Applet";
+            this.trayIcon.Visible = true;
+            this.trayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.trayIcon_MouseDoubleClick);
+            // 
+            // rb_notify_true
+            // 
+            this.rb_notify_true.AutoSize = true;
+            this.rb_notify_true.Location = new System.Drawing.Point(124, 32);
+            this.rb_notify_true.Name = "rb_notify_true";
+            this.rb_notify_true.Size = new System.Drawing.Size(47, 17);
+            this.rb_notify_true.TabIndex = 1;
+            this.rb_notify_true.TabStop = true;
+            this.rb_notify_true.Text = "True";
+            this.rb_notify_true.UseVisualStyleBackColor = true;
+            // 
+            // rb_notify_false
+            // 
+            this.rb_notify_false.AutoSize = true;
+            this.rb_notify_false.Location = new System.Drawing.Point(177, 31);
+            this.rb_notify_false.Name = "rb_notify_false";
+            this.rb_notify_false.Size = new System.Drawing.Size(50, 17);
+            this.rb_notify_false.TabIndex = 2;
+            this.rb_notify_false.TabStop = true;
+            this.rb_notify_false.Text = "False";
+            this.rb_notify_false.UseVisualStyleBackColor = true;
+            // 
+            // timerUpdate
+            // 
+            this.timerUpdate.Enabled = true;
+            this.timerUpdate.Tick += new System.EventHandler(this.timerUpdate_Tick);
+            // 
+            // lblName
+            // 
+            this.lblName.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(0, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(284, 22);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Final Fantasy XI Online LCD Applet";
+            this.lblName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblNotify
+            // 
+            this.lblNotify.AutoSize = true;
+            this.lblNotify.Location = new System.Drawing.Point(12, 36);
+            this.lblNotify.Name = "lblNotify";
+            this.lblNotify.Size = new System.Drawing.Size(106, 13);
+            this.lblNotify.TabIndex = 4;
+            this.lblNotify.Text = "Minimize Notification:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblNotify);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.rb_notify_false);
+            this.Controls.Add(this.rb_notify_true);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_OnClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.RadioButton rb_notify_true;
+        private System.Windows.Forms.RadioButton rb_notify_false;
+        public System.Windows.Forms.Timer timerUpdate;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblNotify;
+    }
+}
+
---
+++ LogitechLCDFFXI/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LogitechLCDFFXI
+{ 
+    
+    public partial class Form1 : Form
+    {
+        string charName, job, sjob;
+        int lvl, slvl, curEXP, nextEXP;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            reciveInfo("Rosealyne", "RDM", 23, "DNC", 99, 118800, 118800);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Logitech.LogiLcdInit("FFXI", Logitech.LcdType.Mono);
+            trayIcon.BalloonTipText = "Running in tray. Double click tray icon to maximize.";
+            trayIcon.BalloonTipTitle = "FFXI LCD Applet";
+            trayIcon.BalloonTipIcon = ToolTipIcon.Info;
+            trayIcon.Icon = this.Icon;
+            rb_notify_true.Checked = true;
+            Logitech.LogiLcdMonoSetText(1, "     Final Fantasy XI     ");
+            Logitech.LogiLcdMonoSetText(2, "          Online          ");
+            Logitech.LogiLcdMonoSetText(3, "   Awaiting Connection... ");
+        }
+
+        private void Form1_OnClosing(object sender, FormClosingEventArgs e)
+        {
+            Logitech.LogiLcdShutdown();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                trayIcon.Visible = true;
+                if (rb_notify_true.Checked) {
+                    trayIcon.ShowBalloonTip(500);
+                }
+                this.Hide();
+            }
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                trayIcon.Visible = false;
+            }
+        }
+
+        private void trayIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void timerUpdate_Tick(object sender, EventArgs e)
+        {
+            if (Logitech.LogiLcdIsConnected(Logitech.LcdType.Mono) /*|| Logitech.LogiLcdIsConnected(Logitech.LcdType.Color)*/)
+            {
+                Logitech.LogiLcdUpdate();
+            }
+            if (Logitech.LogiLcdIsButtonPressed(Logitech.Buttons.MonoButton0))
+            {
+                Logitech.LogiLcdMonoSetText(0, charName + " " + job + lvl + "/" + sjob + slvl);
+                Logitech.LogiLcdMonoSetText(1, "");
+                Logitech.LogiLcdMonoSetText(2, "EXP:" + curEXP + "/" + nextEXP);
+                Logitech.LogiLcdMonoSetText(3, "");
+                Logitech.LogiLcdMonoSetBackground(EXPbar.expbar[this.expbarselection(curEXP, nextEXP)]);
+            }else if (Logitech.LogiLcdIsButtonPressed(Logitech.Buttons.MonoButton1))
+            {
+                Logitech.LogiLcdMonoSetText(0, "Tab 2");
+                Logitech.LogiLcdMonoSetText(1, "");
+                Logitech.LogiLcdMonoSetText(2, "");
+                Logitech.LogiLcdMonoSetText(3, "");
+            }
+            else if (Logitech.LogiLcdIsButtonPressed(Logitech.Buttons.MonoButton2))
+            {
+                Logitech.LogiLcdMonoSetText(0, "Tab 3");
+                Logitech.LogiLcdMonoSetText(1, "");
+                Logitech.LogiLcdMonoSetText(2, "");
+                Logitech.LogiLcdMonoSetText(3, "");
+            }
+            else if (Logitech.LogiLcdIsButtonPressed(Logitech.Buttons.MonoButton3))
+            {
+                Logitech.LogiLcdMonoSetText(0, "Tab 4");
+                Logitech.LogiLcdMonoSetText(1, "");
+                Logitech.LogiLcdMonoSetText(2, "");
+                Logitech.LogiLcdMonoSetText(3, "");
+            }
+        }
+
+        private void reciveInfo(string charname, string job, int lvl, string sjob, int slvl, int curexp, int nextexp)
+        {
+            while (charname.Length < 15) {
+                charname = charname + " ";
+            }
+            this.charName = charname;
+            this.job = job;
+            this.lvl = lvl;
+            this.sjob = sjob;
+            this.slvl = slvl;
+            this.curEXP = curexp;
+            this.nextEXP = nextexp;
+        }
+
+        double map(int x, int in_min, int in_max, int out_min,int out_max) {
+            return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        }
+
+        private int expbarselection(int expcur, int expneed)
+        {
+            return Convert.ToInt32(System.Math.Round(map(expcur, 0, expneed, 0, 151)));
+        }
+    }
+}
